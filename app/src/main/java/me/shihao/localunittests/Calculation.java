112f7fbe8989e8869cbdb9af1b2d3f5b55b75239
1406504841@qq.com <shi18781829380>
2017-02-27 16:56:56 +0800

    Initial commit

---
+++ app/src/main/java/me/shihao/localunittests/Calculation.java
@@ -0,0 +1,19 @@
+package me.shihao.localunittests;
+
+/**
+ * Created by shihao <1406504841@qq.com> on 2017/2/27.16:18
+ */
+
+public class Calculation {
+
+    /**
+     * 简单模拟平时编写的方法
+     * @param a
+     * @param b
+     * @return
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ app/src/test/java/me/shihao/localunittests/CalculationTest.java
@@ -0,0 +1,14 @@
+package me.shihao.localunittests;
+
+import org.junit.Test;
+
+/**
+ * Created by shihao <1406504841@qq.com> on 2017/2/27.16:40
+ */
+public class CalculationTest {
+    @Test
+    public void add() throws Exception {
+
+    }
+
+}
